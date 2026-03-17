d23a727a720098fa88db488de374a32e5bb24ea5
DIVINEakisa <akisadivine11@gmail.com>
2026-03-17 18:33:13 +0200

    styling

--- my-travel-journal/src/App.tsx
+++ my-travel-journal/src/App.tsx
@@ -1,10 +1,12 @@
 import { Headers } from "./components/Header";
+import MainComponent from "./components/Main";
 import "./App.css";
 
 function App() {
   return (
     <>
       <Headers />
+      <MainComponent />
     </>
   );
 }
