a9b0430fa223bbabd9a24022620c4219c66ca668
DIVINEakisa <akisadivine11@gmail.com>
2026-03-17 17:45:07 +0200

    Header of My travel proj

---
+++ my-travel-journal/src/components/Header.tsx
@@ -0,0 +1,10 @@
+export function Headers() {
+  return (
+    <>
+      <nav>
+        <img src="./assets/Path.png" alt="my travel" />
+        <span className="travel">my travel journal</span>
+      </nav>
+    </>
+  );
+}
