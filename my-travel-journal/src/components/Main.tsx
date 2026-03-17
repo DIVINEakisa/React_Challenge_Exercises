6d445ead41991618d8efc69225862975985feda6
DIVINEakisa <akisadivine11@gmail.com>
2026-03-17 17:49:40 +0200

    Main of My travel proj

--- my-travel-journal/src/components/Main.tsx
+++ my-travel-journal/src/components/Main.tsx
@@ -1,1 +1,26 @@
-export default function Main() {}
+export default function Main() {
+  return (
+    <>
+      <main>
+        <div className="japan">
+          <img
+            src="https://scrimba.com/links/travel-journal-japan-image-url"
+            alt="Japan"
+          />
+        </div>
+        <div className="Australia">
+          <img
+            src="https://scrimba.com/links/travel-journal-australia-image-url"
+            alt="Australia"
+          />
+        </div>
+        <div className="Norway">
+          <img
+            src="https://scrimba.com/links/travel-journal-norway-image-url"
+            alt="Norway"
+          />
+        </div>
+      </main>
+    </>
+  );
+}
