f7750755d4b4bbe45ebf65e449c6b1c5422cb25d
DIVINEakisa <akisadivine11@gmail.com>
2026-03-17 19:41:56 +0200

    header

--- my-travel-journal/src/components/Header.tsx
+++ my-travel-journal/src/components/Header.tsx
@@ -1,8 +1,9 @@
+import Path from "./src/assets/Path.png";
 export function Headers() {
   return (
     <>
       <nav>
-        <img src="./assets/Path.png" alt="my travel" />
+        <img src={Path} alt="my travel" className="image" />
         <span className="travel">my travel journal</span>
       </nav>
     </>
