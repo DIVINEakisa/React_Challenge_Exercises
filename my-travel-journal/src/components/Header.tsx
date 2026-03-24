b458f0fa69da3c76a65a1942f6e78c8c3a3817ea
DIVINEakisa <akisadivine11@gmail.com>
2026-03-24 16:42:16 +0200

    my travel journal

--- my-travel-journal/src/components/Header.tsx
+++ my-travel-journal/src/components/Header.tsx
@@ -1,10 +1,10 @@
 import Path from "../assets/Path.png";
-export function Headers() {
+export function Headers(props) {
   return (
     <>
       <nav>
         <img src={Path} alt="my travel" className="image" />
-        <span className="travel">my travel journal</span>
+        <span className="travel">{props.title}</span>
       </nav>
     </>
   );
--- my-travel-journal/src/components/Main.tsx
+++ my-travel-journal/src/components/Main.tsx
@@ -1,58 +1,13 @@
-export default function MainComponent() {
+export default function MainComponent(props) {
   return (
     <>
       <main>
         <div className="japan">
-          <img
-            src="https://scrimba.com/links/travel-journal-japan-image-url"
-            alt="Japan"
-          />
+          <img src={props.img.src} alt={props.img.alt} />
           <div>
-            <h1>Mount Fuji</h1>
-            <h6>12 Jan,2023 - 24 Jan, 2023</h6>
-            <p>
-              Mount Fuji is the tallest mountain in Japan, standing at 3,776{" "}
-              <br />
-              (12,380 feet). Mount Fuji is the single most popular tourist{" "}
-              <br />
-              site in Japan, for both Japanese and foreign tourists.
-            </p>
-          </div>
-        </div>
-        <div className="Australia">
-          <img
-            src="https://scrimba.com/links/travel-journal-australia-image-url"
-            alt="Australia"
-          />
-          <div>
-            <h1>Sydney Opera House</h1>
-            <h6>27 May, 2023 - 8 Jun, 2023</h6>
-            <p>
-              The Sydney Opera House is a multi-venue performing arts centre in{" "}
-              <br />
-              Sydney. Located on the banks of the Sydney Harbour, it is often{" "}
-              <br />
-              regarded as one of the 20th century's most famous and distinctive{" "}
-              <br />
-              buildings.
-            </p>
-          </div>
-        </div>
-        <div className="Norway">
-          <img
-            src="https://scrimba.com/links/travel-journal-norway-image-url"
-            alt="Norway"
-          />
-          <div>
-            <h1>Geirangerfjord</h1>
-            <h6>01 Oct, 2024 - 18 Nov, 2024</h6>
-            <p>
-              The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og{" "}
-              <br />
-              Romsdal county, Norway. It is located entirely in the Stranda{" "}
-              <br />
-              Municipality.
-            </p>
+            <h1>{props.title}</h1>
+            <h6>{props.date}</h6>
+            <p>{props.text}</p>
           </div>
         </div>
       </main>
