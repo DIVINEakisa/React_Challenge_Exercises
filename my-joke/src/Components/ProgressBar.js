5bdefc5111727829a921cf158a70f3f9ea7eaa01
DIVINEakisa <akisadivine11@gmail.com>
2026-03-24 17:42:13 +0200

    from my joke to progress bar

---
+++ my-joke/src/Components/ProgressBar.js
@@ -0,0 +1,13 @@
+export function ProgressBar(props) {
+  return (
+    <>
+      <h1>{props.title}</h1>
+      <input />
+      <label>
+        Input Percentage
+        <label />{" "}
+      </label>
+      <input type="number" />
+    </>
+  );
+}
